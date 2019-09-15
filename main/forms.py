158871813eb6f08a8b82fe21bf51ff98a1245484
BRO3886 <siddverma1999@gmail.com>
2019-09-15 13:18:00 +0530

    added message box in form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="Name", max_length= 30, required=True )
-    email = forms.EmailField(label="Email", required=True)
-    phone = forms.CharField(label="Phone", max_length= 12, required=True)
+    name = forms.CharField(label="Name", max_length= 30, required=True, help_text="Who are you?" )
+    email = forms.EmailField(label="Email", required=True, help_text="Trust me, I won't spam you with emails.")
+    phone = forms.CharField(label="Phone", max_length= 12, required=True, help_text="I follow the policy 'text first, call later'")
+    message = forms.CharField(label = "Message",  widget=forms.Textarea, help_text='Leave me a message!')
