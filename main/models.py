f984161ef4c87e5ede40fb36189e2392e532ef92
BRO3886 <siddverma1999@gmail.com>
2019-09-15 13:31:38 +0530

    added form model and registered the model to admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class contact(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    message = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
